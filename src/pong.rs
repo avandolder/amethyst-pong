1d23a8e15d992edffaf1ac6adb1ee240dbf70b8e
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-15 23:31:48 -0400

    Added Paddle component

--- src/pong.rs
+++ src/pong.rs
@@ -30,3 +30,32 @@ fn initialize_camera(world: &mut World) {
         .with(transform)
         .build();
 }
+
+pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const PADDLE_WIDTH: f32 = 4.0;
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    fn new(side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
