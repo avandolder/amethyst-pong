3d791738ea7e3f553a271d8aec01c72ca3e32907
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-16 23:16:35 -0400

    Missed files in previous commit

---
+++ src/systems/winner.rs
@@ -0,0 +1,33 @@
+use amethyst::{
+    core::transform::Transform,
+    ecs::prelude::{Join, System, WriteStorage},
+};
+
+use crate::pong::{Ball, ARENA_WIDTH};
+
+pub struct WinnerSystem;
+
+impl<'s> System<'s> for WinnerSystem {
+    type SystemData = (WriteStorage<'s, Ball>, WriteStorage<'s, Transform>);
+
+    fn run(&mut self, (mut balls, mut locals): Self::SystemData) {
+        for (ball, transform) in (&mut balls, &mut locals).join() {
+            let ball_x = transform.translation().x;
+
+            let did_hit = if ball_x.as_f32() <= ball.radius {
+                println!("Player 2 scores!");
+                true
+            } else if ball_x.as_f32() >= ARENA_WIDTH - ball.radius {
+                println!("Player 1 scores!");
+                true
+            } else {
+                false
+            };
+
+            if did_hit {
+                ball.velocity[0] = -ball.velocity[0]; // Reverse Direction
+                transform.set_translation_x(ARENA_WIDTH / 2.0); // Reset Position
+            }
+        }
+    }
+}
