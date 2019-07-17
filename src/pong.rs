64daacdf611c7909acb1e26d0835ebe329f1d245
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-16 21:42:31 -0400

    Added Ball component

--- src/pong.rs
+++ src/pong.rs
@@ -9,7 +9,7 @@ use amethyst::{
 pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
 pub const BALL_VELOCITY_X: f32 = 75.0;
-pub const BALL_VALOCITY_Y: f32 = 50.0;
+pub const BALL_VELOCITY_Y: f32 = 50.0;
 pub const BALL_RADIUS: f32 = 2.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
 pub const PADDLE_WIDTH: f32 = 4.0;
@@ -23,6 +23,9 @@ impl SimpleState for Pong {
         // Load the spritesheet necessary to render the graphics.
         let sprite_sheet_handle = load_sprite_sheet(world);
 
+        world.register::<Ball>();
+
+        initialize_ball(world, sprite_sheet_handle.clone());
         initialize_paddles(world, sprite_sheet_handle);
         initialize_camera(world);
     }
@@ -97,6 +100,38 @@ fn initialize_paddles(world: &mut World, sprite_sheet: Handle<SpriteSheet>) {
         .with(sprite_render.clone())
         .with(Paddle::new(Side::Right))
         .with(right_transform)
+        .build();
+}
+
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Component for Ball {
+    type Storage = DenseVecStorage<Self>;
+}
+
+// Initializes one ball in the middle-ish of the arena.
+fn initialize_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    // Create the translation.
+    let mut local_transform = Transform::default();
+    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    // Assign the sprite for the ball.
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 1, // ball is the second sprite on the sprite sheet
+    };
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
         .build();
 }
 
