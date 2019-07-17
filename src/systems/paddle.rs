fe806e7f738b6daae581edf170c648dd517b8298
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-16 21:28:56 -0400

    Can move paddles

--- src/systems/paddle.rs
+++ src/systems/paddle.rs
@@ -22,13 +22,13 @@ impl<'s> System<'s> for PaddleSystem {
                 Side::Right => input.axis_value("right_paddle"),
             };
             if let Some(mv_amount) = movement {
-                if mv_amount != 0.0 {
-                    let side_name = match paddle.side {
-                        Side::Left => "left",
-                        Side::Right => "right",
-                    };
-                    println!("Side {:?} moving {}", side_name, mv_amount);
-                }
+                let scaled_amount = 1.2 * mv_amount as f32;
+                let paddle_y = transform.translation().y;
+                transform.set_translation_y(
+                    (paddle_y + Float::from(scaled_amount))
+                        .min(Float::from(ARENA_HEIGHT - PADDLE_HEIGHT * 0.5))
+                        .max(Float::from(PADDLE_HEIGHT * 0.5)),
+                );
             }
         }
     }
